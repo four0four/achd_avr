b472197f73469b3237fe1465446b5b59c46a13b6
four0four <shmeeeh@gmail.com>
2016-05-09 15:19:15 -0400

    tab fixes?

--- cpu_tb.v
+++ cpu_tb.v
@@ -4,36 +4,36 @@ module cpu_tb;
 
   reg rst = 0;
   reg clk = 0;
-	wire [7:0] rrdo, rddo, rddi;
-	wire [7:0] S;
-//  reg [15:0] pmem [511:0];
+  wire [7:0] rrdo, rddo, rddi;
+  wire [7:0] S;
+  //reg [15:0] pmem [511:0];
 
   reg [15:0] inst;
 
   initial begin
-//    $readmemh("fetchtest.hex", pmem);
+    //$readmemh("fetchtest.hex", pmem);
     $dumpvars(0, c);
     #5 rst = 1;
     #5 rst = 0;
-		#10 inst = 16'b0101000010100001;
-		#10 inst = 16'b0101000010100010;
-		#10 inst = 16'b0101000010100000;
+    #10 inst = 16'b0101000010100001;
+    #10 inst = 16'b0101000010100010;
+    #10 inst = 16'b0101000010100000;
     #20 $finish;
   end
 
   always #5 clk = !clk;
 
-	avr_cpu c(
-		.CLK(clk),
-		.RST(rst),
-		.instr(inst),
-		.p_addr(),
-		.d_addr(),
-		.S_reg(S),
-		.Rr_do(rrdo),
-		.Rd_do(rddo),
-		.Rd_di(rddi)
-	);
+  avr_cpu c(
+      .CLK(clk),
+      .RST(rst),
+      .instr(inst),
+      .p_addr(),
+      .d_addr(),
+      .S_reg(S),
+      .Rr_do(rrdo),
+      .Rd_do(rddo),
+      .Rd_di(rddi)
+  );
 
 endmodule
 
